251a993d9e0aeb882a3c1d2e60a0825c1bcfdd2a
Eduardofbarreto <fbarreto.eduardo@gmail.com>
2025-08-24 18:36:42 -0300

    formatando

--- script.js
+++ script.js
@@ -20,6 +20,7 @@ function azul(){
     document.getElementById("body").style.backgroundColor = "black";
     document.getElementById("demo").style.padding = "30px";
     document.getElementById("demo").style.margin = "10px"
+    document.getElementById("demo").style.borderRadius = "0px";
 }
 
 function verde(){
@@ -30,4 +31,20 @@ function verde(){
     document.getElementById("demo").style.fontSize = "40px";
     document.getElementById("demo").style.padding = "30px";
     document.getElementById("demo").style.margin = "10px"
+    document.getElementById("body").style.backgroundColor = "yellowgreen";
+}
+
+function branco(){
+    document.getElementById("body").style.backgroundColor = "black";
+    document.getElementById("demo").style.fontSize = "60px";
+    document.getElementById("demo").style.border = "4px solid orange";
+    document.getElementById("demo").style.borderRadius = "0px";
+    document.getElementById("demo").style.fontFamily = "Google Sans Code";
+    document.getElementById("demo").style.color = "black";
+    document.getElementById("demo").style.backgroundColor = "rgb(66, 239, 245)";
+    document.getElementById("demo").style.textAlign = "center";
+    document.getElementById("demo").style.fontStyle = "bold";
+    document.getElementById("demo").style.fontWeight = "700";
+    document.getElementById("demo").style.padding = "30px";
+    document.getElementById("demo").style.margin = "10px";
 }
