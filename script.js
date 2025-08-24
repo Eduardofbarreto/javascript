8c895e93e9744fe7af78f1e32958085cccfe1766
Eduardofbarreto <fbarreto.eduardo@gmail.com>
2025-08-24 17:32:08 -0300

    formatando

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
-document.getElementById("demo").innerHTML = "<h2>Fazendo apenas um teste!</h2>";
-document.getElementById("frase").innerHTML = "<p>Aqui está uma frase usando parágrafo!</p>";
-document.getElementById("aprendendo").innerHTML = "<p>Mais um momento no qual estamos descobrindo algo!</p>";
+
+
+function preto(){
+    document.getElementById("demo").style.fontSize = "25px";
+    document.getElementById("demo").style.color = "red";
+    document.getElementById("demo").style.backgroundColor = "black";
+    document.getElementById("demo").style.textAlign = "left";
+    document.getElementById("body").style.backgroundColor = "white";
+    document.getElementById("demo").style.padding = "30px";
+    document.getElementById("demo").style.margin = "10px"
+}
+
+
+function azul(){
+    document.getElementById("demo").style.fontFamily = 'arial';
+    document.getElementById("demo").style.textAlign = "center";
+    document.getElementById("demo").style.backgroundColor = "blue";
+    document.getElementById("demo").style.border = "1px solid yellow";
+    document.getElementById("demo").style.color = "white";
+    document.getElementById("body").style.backgroundColor = "black";
+    document.getElementById("demo").style.padding = "30px";
+    document.getElementById("demo").style.margin = "10px"
+}
+
+function verde(){
+    document.getElementById("demo").style.textAlign = "right";
+    document.getElementById("demo").style.border = "2px solid white";
+    document.getElementById("demo").style.borderRadius = "30px";
+    document.getElementById("demo").style.backgroundColor = "green";
+    document.getElementById("demo").style.fontSize = "40px";
+    document.getElementById("demo").style.padding = "30px";
+    document.getElementById("demo").style.margin = "10px"
+}
