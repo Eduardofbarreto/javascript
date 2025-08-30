cbf24a4c37469c09a929b6c0a958d097d2d85ca5
Eduardofbarreto <fbarreto.eduardo@gmail.com>
2025-08-30 10:55:34 -0300

    praticando

--- script.js
+++ script.js
@@ -1,55 +1,17 @@
+let numero;
 
+numero = 4;
 
-function preto(){
-    document.getElementById("demo").style.fontSize = "25px";
-    document.getElementById("demo").style.color = "red";
-    document.getElementById("demo").style.backgroundColor = "black";
-    document.getElementById("demo").style.textAlign = "left";
-    document.getElementById("body").style.backgroundColor = "white";
-    document.getElementById("demo").style.padding = "30px";
-    document.getElementById("demo").style.margin = "10px"
-}
-
-
-function azul(){
-    document.getElementById("demo").style.fontFamily = 'arial';
-    document.getElementById("demo").style.textAlign = "center";
-    document.getElementById("demo").style.backgroundColor = "blue";
-    document.getElementById("demo").style.border = "1px solid yellow";
-    document.getElementById("demo").style.color = "white";
-    document.getElementById("body").style.backgroundColor = "black";
-    document.getElementById("demo").style.padding = "30px";
-    document.getElementById("demo").style.margin = "10px"
-    document.getElementById("demo").style.borderRadius = "0px";
-}
-
-function verde(){
-    document.getElementById("demo").style.textAlign = "right";
-    document.getElementById("demo").style.border = "2px solid white";
-    document.getElementById("demo").style.borderRadius = "30px";
-    document.getElementById("demo").style.backgroundColor = "green";
-    document.getElementById("demo").style.fontSize = "40px";
-    document.getElementById("demo").style.padding = "30px";
-    document.getElementById("demo").style.margin = "10px"
-    document.getElementById("body").style.backgroundColor = "yellowgreen";
-    document.getElementById("demo1").innerHTML = "";
-    document.getElementById("demo2").innerHTML = "Com a cor verde";
-
-}
-
-function branco(){
-    document.getElementById("body").style.backgroundColor = "black";
-    document.getElementById("demo").style.fontSize = "60px";
-    document.getElementById("demo").style.border = "4px solid orange";
-    document.getElementById("demo").style.borderRadius = "0px";
-    document.getElementById("demo").style.fontFamily = "Google Sans Code";
-    document.getElementById("demo").style.color = "black";
-    document.getElementById("demo").style.backgroundColor = "rgb(66, 239, 245)";
-    document.getElementById("demo").style.textAlign = "center";
-    document.getElementById("demo").style.fontStyle = "bold";
-    document.getElementById("demo").style.fontWeight = "700";
-    document.getElementById("demo").style.padding = "30px";
-    document.getElementById("demo").style.margin = "10px";
-    document.getElementById("demo1").innerHTML = "Fazendo um teste!";
-    document.getElementById("demo1").style.color = "white";
+switch(numero){
+    case 1:
+        document.getElementById("demo").innerHTML = "Domingo";
+    break;
+    case 2:
+        document.getElementById("demo").innerHTML = "Segunda-feira";
+    break;
+    case 3:
+        document.getElementById("demo").innerHTML = "Terça-feira";
+    break;
+    default:
+        document.getElementById("demo").innerHTML = "Nenhum valor válido!";
 }
