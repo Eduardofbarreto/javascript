e67febd6e8780e66ea53c68a379a838a66a58adb
Eduardofbarreto <fbarreto.eduardo@gmail.com>
2025-09-27 16:58:16 -0300

    resolva este problema de lógica

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const form = document.getElementById('meuFormulario');
 const inputNum1 = document.getElementById('num1');
 const inputNum2 = document.getElementById('num2');
+const inputResultado = document.getElementById('options');
 
 const divMensagem = document.getElementById('resultado');
 
@@ -8,11 +9,20 @@ const divMensagem = document.getElementById('resultado');
 form.addEventListener('submit', function(event){
     event.preventDefault();
 
+
+    const optionsDigitada = inputOptions.value;
     const num1Digitado = inputNum1.value;
     const num2Digitado = inputNum2.value;
     const soma = parseInt(num1Digitado) + parseInt(num2Digitado);
+    const subt = parseInt(num1Digitado) - parseInt(num2Digitado);
 
-    const resultado = `${num1Digitado} + ${num2Digitado} = ${soma}`;
+    if(optionsDigitada == 'soma'){
+        const resultado = `${num1Digitado} + ${num2Digitado} = ${soma}`;
+    }else{
+        const resultado = `${num1Digitado} - ${num2Digitado} = ${subt}`;
+    }
+    
+    
 
     divMensagem.textContent = resultado;
 
