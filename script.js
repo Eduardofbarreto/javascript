1d2881379e1eecedf2f68550ab4b2e15bd2102b8
Eduardofbarreto <fbarreto.eduardo@gmail.com>
2025-09-27 20:54:29 -0300

    teste

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
-function greet(name, age){
-    return "Hello " + name + "! You are " + age + " years old.";
-}
-document.getElementById("demo").innerHTML = greet("Eduardo", 38);
+document.getElementById("meuFormulario").addEventListener("submit", function(event){
+    event.preventDefault();
 
-function soma(a, b){
-    return a + b;
-}
+    const num1 = parseFloat(document.getElementById("num1").value);
+    const num2 = parseFloat(document.getElementById('num2').value);
+    const operacao = document.getElementById('options').value;
+    const resultadoDiv = document.getElementById('resultado');
 
-document.getElementById("demo1").innerHTML = soma(6, 6);
+    let resultado;
+
+    if(isNaN(num1) || isNaN(num2)){
+        resultadoDiv.textContent = "Por favor, insira dois números válidos! Caso contrário, nada funcionará!";
+        return;
+    }
+
+    switch(operacao){
+        case 'soma':
+            resultado = num1 + num2;
+            break;
+        case 'subt':
+            resultado = num1 - num2;
+            break;
+        default:
+            resultadoDiv.textContent = 'Selecione uma operação.';
+            return;
+    }
+
+    resultadoDiv.textContent = `Resultado: ${resultado}`;
+});
