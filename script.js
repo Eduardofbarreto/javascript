84c79ba205abb5b2fcbfe30b7ae0661b5c7a96d2
Eduardofbarreto <fbarreto.eduardo@gmail.com>
2025-09-27 21:11:13 -0300

    problema de lógica para resolver

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ document.getElementById("meuFormulario").addEventListener("submit", function(eve
     const num2 = parseFloat(document.getElementById('num2').value);
     const operacao = document.getElementById('options').value;
     const resultadoDiv = document.getElementById('resultado');
+    const mensagemFinal = document.getElementById('mensagem');
 
     let resultado;
 
@@ -15,15 +16,27 @@ document.getElementById("meuFormulario").addEventListener("submit", function(eve
 
     switch(operacao){
         case 'soma':
+            `A soma de ${num1} + ${num2}`;
             resultado = num1 + num2;
             break;
         case 'subt':
             resultado = num1 - num2;
+            break;
+        case 'mult':
+            resultado = num1 * num2;
+            break;
+        case 'div':
+            if(num2 > 0){
+                resultado = num1 / num2;
+            }else{
+                resultado = 'Você não pode dividir por 0!';
+            }
             break;
         default:
             resultadoDiv.textContent = 'Selecione uma operação.';
             return;
     }
 
-    resultadoDiv.textContent = `Resultado: ${resultado}`;
+    resultadoDiv.textContent = `Resultado: ${resultado} `;
+    mensagemFinal.textContent = `${num1} e ${num2}`;
 });
