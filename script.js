caddef707de93932c768bbf061b13761ef1253d0
Eduardofbarreto <fbarreto.eduardo@gmail.com>
2025-09-29 15:30:14 -0300

    entendendo conceitos

--- script.js
+++ script.js
@@ -1,31 +1,16 @@
-const form = document.getElementById('meuFormulario');
-const inputNum1 = document.getElementById('num1');
-const inputNum2 = document.getElementById('num2');
-const inputSoma = document.getElementById('soma');
-const inputSUbt = document.getElementById('subt');
+document.getElementById('meuFormulario').addEventListener('submit', 
+    function(event){
+        event.preventDefault();
 
-const divMensagem = document.getElementById('resultado');
+        const nomeElemento = document.getElementById('nome');
+        const idadeElemento = document.getElementById('idade');
+
+        const nomeDigitado = nomeElemento.value;
+        const idadeDigitada = idadeElemento.value;
+
+        const mensagemhtml = document.getElementById('mensagem');
 
 
-form.addEventListener('submit', function(event){
-    event.preventDefault();
-
-    const num1Digitado = inputNum1.value;
-    const num2Digitado = inputNum2.value;
-    const soma = parseInt(num1Digitado) + parseInt(num2Digitado);
-    const subt = parseInt(num1Digitado) - parseInt(num2Digitado);
-
-    if(optionsDigitada == 'soma'){
-        const divMensagem = `${num1Digitado} + ${num2Digitado} = ${soma}`;
-    }else{
-        const divMensagem = `${num1Digitado} - ${num2Digitado} = ${subt}`;
+        mensagemhtml.textContent = `Olá ${nomeDigitado}! Você tem ${idadeDigitada} anos!`;
     }
-    
-    
-
-    divMensagem.textContent = resultado;
-
-    divMensagem.style.display = 'block';
-
-    inputNome.value = '';
-});
+);
