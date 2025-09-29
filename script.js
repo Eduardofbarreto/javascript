f1ce4248704e1b56ab451401fecfdac488c4af94
Eduardofbarreto <fbarreto.eduardo@gmail.com>
2025-09-29 16:25:47 -0300

    innerHTML

--- script.js
+++ script.js
@@ -1,16 +1,2 @@
-document.getElementById('meuFormulario').addEventListener('submit', 
-    function(event){
-        event.preventDefault();
-
-        const nomeElemento = document.getElementById('nome');
-        const idadeElemento = document.getElementById('idade');
-
-        const nomeDigitado = nomeElemento.value;
-        const idadeDigitada = idadeElemento.value;
-
-        const mensagemhtml = document.getElementById('mensagem');
-
-
-        mensagemhtml.textContent = `Olá ${nomeDigitado}! Você tem ${idadeDigitada} anos!`;
-    }
-);
+document.getElementById('demo').innerHTML = 'Olá mundo!';
+document.getElementById('demo1').innerHTML = "Meu nome é Eduardo F. Barreto!";
