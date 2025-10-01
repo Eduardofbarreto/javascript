a5da9b9432f097f1409ec6089677c3c85c63da14
Eduardofbarreto <fbarreto.eduardo@gmail.com>
2025-10-01 11:00:53 -0300

    seguindo

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
-const tagsParaAlterar = document.getElementsByClassName('texto');
+function validarFormulario(){
+    const nomeCliente = 
+    document.getElementById('nome').value.trim();
 
-for(let i = 0; i < tagsParaAlterar.length; i++){
-    tagsParaAlterar[i].innerHTML = 'Eduardo';
+    const isVegetariano = 
+    document.getElementById('vegetariano').value;
+    const isDieta =
+    document.getElementById('dieta').value;
+    const isPagamento =
+    document.getElementById('pagamento').value;
+
+    if(nomeCliente === ''){
+        alert('O nome do cliente é obrigatório!');
+        return false;
+    }
+
+    console.log('Nome do cliente:', nomeCliente);
+    console.log('É vegetariano: ');
+    if(isVegetariano === 'sim'){
+        console.log('Sugestão de prato: Macarrão!');
+    }else{
+        console.log('Carne grelhada.');
+    }
 }
