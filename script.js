871f246413ab767da2962cd51de970b54a7eb48b
Eduardofbarreto <fbarreto.eduardo@gmail.com>
2025-10-01 10:35:39 -0300

    pratos de restaurante com if

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const tagsParaAlterar = document.getElementsByClassName('texto');
 
 for(let i = 0; i < tagsParaAlterar.length; i++){
-    tagsParaAlterar[i].innerHTML = 'Conteúdo alterado pelo JS!';
+    tagsParaAlterar[i].innerHTML = 'Eduardo';
 }
